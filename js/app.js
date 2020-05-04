6f4d92854cb05af027b6a72b16febc18c200fc22
Lucas Ventuala <ventualalucas@gmail.com>
2020-05-03 23:58:36 -0300

    asignacion random de cartas y evento al hacer click

--- js/app.js
+++ js/app.js
@@ -1,7 +1,50 @@
+var cartas = []; // todas las cartas que componen la jugada
+var imagenes = []; // todas las imagenes disponibles para cargar en las cartas
 
-
+var max_img_ls = 36;
+for(var i = 1; i<=max_img_ls; i++) {
+	imagenes.push({
+		dir: './images/los_simpson/img'+i+'.png'
+	});
+}
 
 selectDificultad(); 
+
+function initCartas() {
+	//var item = cartas[Math.floor(Math.random() * cartas.length)];
+	var aux_cartas = [];
+	var aux_imagenes = [];
+	while (cartas.length > 0) {
+		// recupero carta 1
+		var indice_c1 = Math.floor(Math.random() * cartas.length); 
+		var carta_c1 = cartas.splice(indice_c1,1); 
+
+		// recupero carta 2
+		var indice_c2 = Math.floor(Math.random() * cartas.length); 
+		var carta_c2 = cartas.splice(indice_c2,1); 
+
+		// recupero imagen
+		var indice_img = Math.floor(Math.random() * imagenes.length); 
+		var img = imagenes.splice(indice_img, 1);
+
+		// asigno imagen a las cartas
+		carta_c1[0].dir = img[0].dir; 
+		carta_c2[0].dir = img[0].dir; 
+
+		// agrego a los arreglos auxiliares para no perder la info
+		aux_cartas = aux_cartas.concat(carta_c1); 
+		aux_cartas = aux_cartas.concat(carta_c2); 
+		aux_imagenes = aux_imagenes.concat(img); 
+
+		// configuro la carta en los div's correspondientes
+		$("#"+carta_c1[0].id+" .flip-card-front").css('background-image', 'url("'+carta_c1[0].dir+'")');
+		$("#"+carta_c2[0].id+" .flip-card-front").css('background-image', 'url("'+carta_c2[0].dir+'")');
+	}
+
+	imagenes = imagenes.concat(aux_imagenes); 
+
+	//$(id+" .flip-card-front").css('background-image', 'url("./images/los_simpson/img1.png")');
+}
 
 /**
  * Modifica la cantidad de cartas con la que se va a jugar
@@ -33,11 +76,13 @@ function cargarCartas(n,m) {
 		html += '<div id="id-fila+'+f+'" class="fila">'; 
 
 		for(var c = 0; c<n; c++) {
+			var id = 'id_c'+f+c; 
+
 			html += '<div id="id-columna-"'+c+' class="columna">'; 
 
 			html += '<div class="flip-card">';
 
-			html += '		<div id="id_c'+f+c+'" class="flip-card-inner">';
+			html += '		<div id="'+id+'" class="flip-card-inner">';
 			html += '			<div class="card flip-card-front">'; 
 			//html += '				<div>'; 
 			//html += '					<img src="images/los_simpson/img1.png">';
@@ -54,6 +99,13 @@ function cargarCartas(n,m) {
 			html += '</div>';
 
 			html += '</div>'
+
+			// agrego carta al arreglo global
+			cartas.push({
+				'id' : id, 
+				'estado' : 'back',  // back o front
+				'dir': ""
+			});
 		}
 
 		html += '</div>'
@@ -68,9 +120,18 @@ function cargarCartas(n,m) {
 	$('.columna').css('padding','2px'); 
 	$('.flip-card').css('height','100%'); 
 
-	for(var f = 0; f<m; f++) {
-		for(var c= 0; c<n; c++) {
-			$('#id_c'+f+c).css('transform', 'rotateY(180deg)');
-		}
+	for(var i = 0; i<cartas.length; i++){
+		c = cartas[i];
+		$("#"+c.id).css('transform', 'rotateY(180deg)');
+		//$(id+" .flip-card-front").css('background-image', 'url("./images/los_simpson/img1.png")');
+		$("#"+c.id).click(function () {
+			girarCarta(this);
+		});
 	}
+
+	initCartas(); 
+}
+
+function girarCarta(elem) {
+	$("#"+elem.id).css('transform', 'rotateY(360deg)');
 }
